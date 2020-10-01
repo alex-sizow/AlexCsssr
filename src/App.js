6efc87911477a564314fdcd5e4d7214240ec724b
Alex <alex_sizow@outlook.com>
2020-10-01 17:10:13 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './App.css'
-import product
+import productData from './products.json'
 
 function App() {
 	return (
 		<div className='App'>
 			<div class='header'>Список товаров</div>
+			<ul class='list'>
+				{productData.map((product) => {
+					if (product.id < 4) {
+						return <li>{product.name}</li>
+					}
+				})}
+			</ul>
 		</div>
 	)
 }
